6663441997842e0771fb11c78ad5ad1c45ea997f
Rheganandar Bagas <rheganandar.bi@gmail.com>
2026-03-05 12:52:20 +0700

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-portfolio/',
+  base: 'http://regis22-kda.github.io/personal-portfolio/',
 })
